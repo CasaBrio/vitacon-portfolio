62b5bec8e54e3886eb629e47e7be3c77f4a3df29
Vitacon <sperandeoluis@gmail.com>
2026-08-03 23:59:01 -0300

    Update comunicados.js

--- comunicados.js
+++ comunicados.js
@@ -60,4 +60,11 @@ const COMUNICADOS = [
    linkTexto: "Planilha Estoque"
   }
 ];
-
+  {
+    data: "03/08/2026",
+    titulo: "Vitacon - News",
+    texto: "aqui vc encontra as mais recentes novidades sobre a Vitacon",
+    imagem: "https://drive.google.com/file/d/1-BUKqp5vUrk2AIvsjMrZ9DdoUbDwjfs9/view?usp=drive_link",
+    link: "https://empreendimentos.vitacon.com.br/vitacon-news-julho-2026",
+    linkTexto: "Vitacon News"
+  },
